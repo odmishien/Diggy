fd3fe7ab7c900f15de9570594dd0da0f2a90ac9c
odmishien <tmtm.yagi.007@gmail.com>
2019-12-08 22:40:05 +0900

    loading gif

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,16 @@
-const digButton = document.querySelector("#digBtn");
+const digButton = document.querySelector("#dig");
 let happinessVal,
   danceabilityVal,
   loudnessVal;
 
 digButton.addEventListener("click", () => {
-  happinessVal = document.querySelector("#happiness").value;
-  danceabilityVal = document.querySelector("#danceability").value;
-  loudnessVal = document.querySelector("#loudness").value;
+  const loading = document.querySelector('#loading');
+  const contents = document.querySelector('#contents');
+  loading.classList.toggle('hidden');
+  contents.classList.toggle('hidden');
+  window.addEventListener('load', () => {
+    loading.classList.toggle('hidden');
+    contents.classList.toggle('hidden');
+  })
 });
 
-const loading = document.querySelector('#loading');
-const contents = document.querySelector('#contents');
-
-window.addEventListener('load', () => {
-  loading.classList.toggle('hidden');
-  contents.classList.toggle('hidden')
-})
